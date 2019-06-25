442a1091cad6ac05259c020ba6a43ce077e2c150
Dave Harned <dharned@gmail.com>
2019-06-25 13:00:54 -0500

    initialize api and test db connection

---
+++ api/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const couchbase = require('couchbase');
+
+// initialize express middleware
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize and verify DB connection
+const cluster = new couchbase.Cluster('couchbase://localhost');
+cluster.authenticate('admin', 'admin1');
+const bucket = cluster.openBucket('guitars');
+bucket.on('error', function(err) {
+  console.log('CONNECT ERROR:', err);
+});
+bucket.on('connect', function() {
+  console.log('connected to db');
+});
+module.exports.bucket = bucket;
+
+const server = app.listen(3000, () => {
+  console.log('Listening on port %s...', server.address().port);
+});
