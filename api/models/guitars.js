baf76a919e16ce18b1f7e9959496a217696c9fc6
Dave Harned <dharned@gmail.com>
2019-06-25 13:34:58 -0500

    build and test post endpoint

--- api/models/guitars.js
+++ api/models/guitars.js
@@ -1,11 +1,32 @@
 const uuid = require('uuid');
 const N1qlQuery = require('couchbase').N1qlQuery;
-const bucket = require('./app').bucket;
+const bucket = require('../app').bucket;
 
 const guitars = {
   getAll: () => {},
   getById: () => {},
-  postNew: () => {},
+  postNew: (data, cb) => {
+    let guitar = {
+      specs: {
+        color: data.color,
+        construction: data.construction,
+        make: data.make,
+        model: data.model,
+        year: data.year
+      },
+      timestamp: new Date(),
+      type: data.type,
+      user: data.user
+    };
+    let id = data.id ? data.id : uuid.v4();
+    bucket.upsert(id, guitar, (err, result) => {
+      if (err) {
+        console.log(err);
+        return cb(err, null);
+      }
+      cb(null, result);
+    });
+  },
   updateById: () => {},
   deleteById: () => {}
 };
--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,16 @@
 const guitars = require('./models/guitars');
 
-const appRouter = app => {};
+const appRouter = app => {
+  // POST new guitar
+  app.post('/guitars', (req, res) => {
+    guitars.postNew(req.body, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).send(err);
+      }
+      res.send(result);
+    });
+  });
+};
 
 module.exports = appRouter;
